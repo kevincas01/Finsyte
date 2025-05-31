dc474d866b33786ea9cb15fca0f4db35cdad01a4
Kevin Castro <kcapupp@princeton.edu>
2025-05-30 22:14:14 -0400

    Signup form component

---
+++ app/Components/Auth/SignUpForm.tsx
@@ -0,0 +1,44 @@
+import GradientButton from "../Buttons/GradientButton";
+import PasswordInput from "../Inputs/PasswordInput";
+import TextInput from "../Inputs/TextInput";
+import GoogleButton from "./GoogleButton";
+interface SignUpFormProps {
+  onTabSwitch: () => void;
+}
+const SignUpForm = ({ onTabSwitch }: SignUpFormProps) => {
+  const handleSignIn = () => {};
+  return (
+    <div className="p-10 flex flex-col gap-4 my-auto">
+      <div className="text-center">
+        <h2 className="font-semibold text-3xl">Sign Up</h2>
+      </div>
+
+      <TextInput
+        label="Email Address"
+        placeholder="Enter your email"
+        icon="email"
+      />
+      <PasswordInput />
+      <GradientButton text="Sign Up" onClickFunction={handleSignIn} />
+
+      <div className="flex items-center gap-4 w-full text-gray-500 text-sm">
+        <div className="flex-grow border-t border-gray-300" />
+        <span className="whitespace-nowrap">Or Continue with</span>
+        <div className="flex-grow border-t border-gray-300" />
+      </div>
+      <GoogleButton />
+
+      <p className="text-center">
+        Have an account?
+        <button
+          className="text-primaryBlue font-medium ml-1 cursor-pointer"
+          onClick={onTabSwitch}
+        >
+          Sign In
+        </button>
+      </p>
+    </div>
+  );
+};
+
+export default SignUpForm;

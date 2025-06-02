bd72ce00a45bbfabdf06ae3191215ddc3fe6c702
Kevin Castro <kcapupp@princeton.edu>
2025-06-01 23:55:18 -0400

    Gradient button- add padding and flex properties

--- app/Components/Buttons/GradientButton.tsx
+++ app/Components/Buttons/GradientButton.tsx
@@ -23,8 +23,8 @@ const GradientButton = ({
       type={type}
       onClick={onClick}
       disabled={disabled || loading}
-      className={`cursor-pointer text-center text-white rounded-md text-lg font-medium py-1 transition duration-200 ease-in-out 
-      bg-gradient-to-r from-primaryBlue to-primaryGreen hover:opacity-80 
+      className={`cursor-pointer text-center text-white rounded-md text-lg font-medium px-4 py-2 transition duration-200 ease-in-out 
+      bg-gradient-to-r from-primaryBlue to-primaryGreen hover:opacity-80 flex gap-2 justify-center items-center
       ${disabled || loading ? "opacity-50 cursor-not-allowed" : ""} 
       ${className}`}
       aria-busy={loading}

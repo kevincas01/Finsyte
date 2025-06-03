6e416d82add8ec3651d5e42a3d8c721082616421
Kevin Castro <kcapupp@princeton.edu>
2025-06-03 15:45:43 -0400

    Text Input- Add search icon

--- app/Components/Inputs/TextInput.tsx
+++ app/Components/Inputs/TextInput.tsx
@@ -1,17 +1,19 @@
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 interface TextInputProps {
   label: string;
   placeholder: string;
-  icon?: "email" | "lock" | "person";
+  icon?: "email" | "lock" | "person"|"search";
 }
 
 const iconMap = {
   email: EmailOutlinedIcon,
   lock: LockOutlinedIcon,
   person: PersonOutlineIcon,
+  search:SearchOutlinedIcon
 };
 
 const TextInput = ({ label, placeholder, icon }: TextInputProps) => {
@@ -23,7 +25,7 @@ const TextInput = ({ label, placeholder, icon }: TextInputProps) => {
         <label className="block mb-1 text-sm font-medium">{label}</label>
       )}
       <div className="flex items-center border border-gray-300 rounded-lg px-3 py-2 bg-white focus-within:ring-2 focus-within:ring-blue-500">
-        {IconComponent && <IconComponent className="text-gray-800 mr-2" />}
+        {IconComponent && <IconComponent className="text-gray-500 mr-2" />}
         <input
           type="text"
           placeholder={placeholder}

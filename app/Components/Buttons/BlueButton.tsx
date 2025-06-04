28e5ab654976689b6090965d38eafad7ffa4bdc3
Kevin Castro <kcapupp@princeton.edu>
2025-06-03 23:34:10 -0400

    Blue button create + center and grow flex on neutral button

--- app/Components/Buttons/BlueButton.tsx
+++ app/Components/Buttons/BlueButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { ReactNode } from "react";
 
-interface NeutralButtonProps {
+interface BlueButtonProps {
   children: ReactNode;
   onClick: () => void;
   type?: "button" | "submit" | "reset";
@@ -10,21 +10,21 @@ interface NeutralButtonProps {
   className?: string;
 }
 
-const NeutralButton = ({
+const BlueButton = ({
   children,
   onClick,
   type = "button",
   disabled = false,
   loading = false,
   className = "",
-}: NeutralButtonProps) => {
+}: BlueButtonProps) => {
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={disabled || loading}
-      className={`cursor-pointer text-center text-black rounded-md font-medium px-4 py-2 transition duration-200 ease-in-out 
-      bg-white border border-gray-200 hover:bg-gray-100 flex gap-2 flex-1 justify-center
+      className={`cursor-pointer text-center text-primaryBlue rounded-md font-medium px-4 py-2 transition duration-200 ease-in-out 
+      bg-primaryBlue/20 border border-gray-200 hover:bg-primaryBlue/30  flex gap-2 flex-1 justify-center
       ${disabled || loading ? "opacity-50 cursor-not-allowed" : ""} 
       ${className}`}
       aria-busy={loading}
@@ -34,4 +34,4 @@ const NeutralButton = ({
   );
 };
 
-export default NeutralButton;
+export default BlueButton;

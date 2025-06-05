8124df94b47765f96dc1d1528173d7b1e6169a1b
Kevin Castro <kcapupp@princeton.edu>
2025-06-04 23:59:22 -0400

    Fix color utils

--- app/Utils/colors.ts
+++ app/Utils/colors.ts
@@ -6,10 +6,10 @@ export const getGoalColor = (percentage: number): string => {
 
 export const getBudgetColor = (percentage: number): string => {
   if (percentage < 80) {
-    return "text-green-600";
+    return "bg-green-600";
   } else if (percentage >= 80 && percentage <= 100) {
-    return "text-yellow-600";
+    return "bg-yellow-600";
   } else {
-    return "text-red-600";
+    return "bg-red-600";
   }
 };

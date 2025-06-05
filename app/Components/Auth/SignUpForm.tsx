e3a884c677007acf3b8af37b4e6dfa7a5808898a
Kevin Castro <kcapupp@princeton.edu>
2025-06-04 23:14:31 -0400

    Sign up form- add states for login info

--- app/Components/Auth/SignUpForm.tsx
+++ app/Components/Auth/SignUpForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import GradientButton from "../Buttons/GradientButton";
 import PasswordInput from "../Inputs/PasswordInput";
 import TextInput from "../Inputs/TextInput";
@@ -6,7 +8,12 @@ interface SignUpFormProps {
   onTabSwitch: () => void;
 }
 const SignUpForm = ({ onTabSwitch }: SignUpFormProps) => {
-  const handleSignIn = () => {};
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = () => {
+    console.log({ email, password });
+  };
   return (
     <div className="p-10 flex flex-col gap-4 my-auto">
       <div className="text-center">
@@ -17,11 +24,19 @@ const SignUpForm = ({ onTabSwitch }: SignUpFormProps) => {
         label="Email Address"
         placeholder="Enter your email"
         icon="email"
+        value={email}
+        onChange={(value) => {
+          setEmail(value);
+        }}
       />
-      <PasswordInput />
-      <GradientButton  onClick={handleSignIn} >
-      Sign Up
-      </GradientButton>
+      <PasswordInput
+        value={password}
+        onChange={(value) => {
+          setPassword(value);
+        }}
+      />
+      
+      <GradientButton onClick={handleSignIn}>Sign Up</GradientButton>
 
       <div className="flex items-center gap-4 w-full text-gray-500 text-sm">
         <div className="flex-grow border-t border-gray-300" />

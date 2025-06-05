2f37df027b5538c040a0caffd866ced83eca7c61
Kevin Castro <kcapupp@princeton.edu>
2025-06-04 23:01:30 -0400

    Transactions Filter component

---
+++ app/Components/Transactions/TransactionsFilter.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { TransactionCategories } from "@/app/Constants/transactions";
+import TextInput from "../Inputs/TextInput";
+import DropdownInput from "../Inputs/DropdownInput";
+
+interface TransactionsFilterProps {
+  search: string;
+  onSearchChange: (value: string) => void;
+  category: string;
+  onCategoryChange: (value: string) => void;
+  account: string;
+  onAccountChange: (value: string) => void;
+}
+
+const TransactionsFilter = ({
+  search,
+  onSearchChange,
+  category,
+  onCategoryChange,
+  account,
+  onAccountChange,
+}: TransactionsFilterProps) => {
+  const categoryOptions = TransactionCategories.map((c) => ({
+    label: c,
+    value: c,
+  }));
+  const accounts = ["Chase Checking", "Capital One Credit","Wells Fargo Debit"]; // Replace with real values if needed
+
+  const accountOptions = accounts.map((a) => ({ label: a, value: a }));
+
+  return (
+    <div className="bg-white p-6 shadow-card flex flex-row gap-5 rounded-md">
+      <TextInput
+        label="Search"
+        placeholder="Search Transactions"
+        icon="search"
+        value={search}
+        onChange={(value) => {
+          onSearchChange(value);
+        }}
+      />
+      <DropdownInput
+        label="Category"
+        options={[
+          { label: "All Categories", value: "All" },
+          ...categoryOptions,
+        ]}
+        value={category}
+        onChange={(option) => onCategoryChange(option as string)}
+      />
+      <DropdownInput
+        label="Account"
+        options={[{ label: "All Accounts", value: "All" }, ...accountOptions]}
+        value={account}
+        onChange={(option) => onAccountChange(option as string)}
+      />
+    </div>
+  );
+};
+export default TransactionsFilter;

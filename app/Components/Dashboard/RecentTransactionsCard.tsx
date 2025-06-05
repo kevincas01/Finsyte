75bf90a5c4c8207dccb7289965ed8a09e09948bd
Kevin Castro <kcapupp@princeton.edu>
2025-06-04 22:38:23 -0400

    Use Mock transactions list

--- app/Components/Dashboard/RecentTransactionsCard.tsx
+++ app/Components/Dashboard/RecentTransactionsCard.tsx
@@ -1,61 +1,7 @@
-import { TransactionCategoryColors } from "@/app/Constants/transactions";
-import { TransactionCategory } from "@/app/Types/transactions";
-
-const TransactionsList: {
-  merchant: string;
-  category: TransactionCategory;
-  date: string;
-  total: number;
-}[] = [
-  {
-    merchant: "Starbucks",
-    category: "Food & Drink",
-    date: "2024-01-25",
-    total: 5.45,
-  },
-  {
-    merchant: "Netflix",
-    category: "Entertainment",
-    date: "2024-01-24",
-    total: 15.99,
-  },
-  {
-    merchant: "Uber",
-    category: "Travel",
-    date: "2024-01-23",
-    total: 18.75,
-  },
-  {
-    merchant: "Amazon",
-    category: "Shopping",
-    date: "2024-01-22",
-    total: 54.32,
-  },
-  {
-    merchant: "Verizon Wireless",
-    category: "Utilities",
-    date: "2024-01-21",
-    total: 89.99,
-  },
-  {
-    merchant: "Trader Joe's",
-    category: "Groceries",
-    date: "2024-01-20",
-    total: 76.45,
-  },
-  {
-    merchant: "Shell",
-    category: "Gas",
-    date: "2024-01-19",
-    total: 42.18,
-  },
-  {
-    merchant: "Spotify",
-    category: "Entertainment",
-    date: "2024-01-18",
-    total: 9.99,
-  },
-];
+import {
+  mockTransactions,
+  TransactionCategoryColors,
+} from "@/app/Constants/transactions";
 
 const RecentTransactionsCard = () => {
   return (
@@ -67,12 +13,11 @@ const RecentTransactionsCard = () => {
         </button>
       </div>
       <div></div>
-      {TransactionsList.map((transaction, index) => (
+      {mockTransactions.map((transaction, index) => (
         <div key={index} className="flex items-center justify-between p-3">
           <div className="flex items-center">
             <div className="w-10 h-10 bg-gray-100 rounded-full flex items-center justify-center mr-3">
               <span className="text-gray-600 text-sm">
-                {" "}
                 {transaction.merchant[0]}
               </span>
             </div>
@@ -88,9 +33,16 @@ const RecentTransactionsCard = () => {
             </div>
           </div>
           <div className="text-right">
-            <p className="font-semibold text-red-500">
-              -${transaction.total.toFixed(2)}
-            </p>
+            {transaction.amount > 0 ? (
+              <p className="font-semibold text-green-500">
+                +${transaction.amount.toFixed(2)}
+              </p>
+            ) : (
+              <p className="font-semibold text-red-500">
+                -${(-1 * transaction.amount).toFixed(2)}
+              </p>
+            )}
+
             <p className="text-sm text-gray-500">
               {new Date(transaction.date).toLocaleDateString("en-US", {
                 month: "short",

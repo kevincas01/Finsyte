ff8e0d8143da319eb506c459d540d32a77dc451e
Kevin Castro <kcapupp@princeton.edu>
2025-06-09 22:27:51 -0400

    Simplify budget page

--- app/(authenticated)/budgets/page.tsx
+++ app/(authenticated)/budgets/page.tsx
@@ -1,68 +1,12 @@
-import BudgetCard from "@/app/Components/Budgets/BudgetCard";
 import BudgetHeader from "@/app/Components/Budgets/BudgetHeader";
-import BudgetOverview from "@/app/Components/Budgets/BudgetOverview";
-
-interface Budget {
-  title: string;
-  description?: string;
-  currentAmount: number;
-  budgetAmount: number;
-}
-const exampleBudgets: Budget[] = [
-  {
-    title: "Groceries",
-    description: "Monthly food and household essentials",
-    currentAmount: 520,
-    budgetAmount: 500,
-  },
-  {
-    title: "Food & Drink",
-    description: "Restaurants, takeout, and cafes",
-    currentAmount: 150,
-    budgetAmount: 200,
-  },
-  {
-    title: "Travel",
-    description: "Gas, rideshare, and public transport",
-    currentAmount: 95.75,
-    budgetAmount: 150,
-  },
-  {
-    title: "Entertainment",
-    description: "Movies, games, events, and subscriptions",
-    currentAmount: 80,
-    budgetAmount: 120,
-  },
-  {
-    title: "Utilities",
-    description: "Electricity, water, internet, etc.",
-    currentAmount: 180.2,
-    budgetAmount: 200,
-  },
-];
+import BudgetPeriodDisplay from "@/app/Components/Budgets/BudgetPeriodDisplay";
+import { exampleBudgets } from "@/app/Constants/budgets";
 
 const BudgetsPage = () => {
-  const totalBudgetAmount = exampleBudgets.reduce(
-    (totalAmount, budget) => totalAmount + budget.budgetAmount,
-    0
-  );
-
-  const totalSavedAmount = exampleBudgets.reduce(
-    (totalAmount, budget) => totalAmount + budget.currentAmount,
-    0
-  );
   return (
     <div className="flex flex-col gap-5">
       <BudgetHeader />
-      <BudgetOverview
-        totalBudgetAmount={totalBudgetAmount}
-        totalSavedAmount={totalSavedAmount}
-      />
-      <div className="grid grid-cols-2 gap-5">
-        {exampleBudgets.map((budget) => (
-          <BudgetCard key={budget.title} budget={budget} />
-        ))}
-      </div>
+      <BudgetPeriodDisplay budgets={exampleBudgets} />
     </div>
   );
 };

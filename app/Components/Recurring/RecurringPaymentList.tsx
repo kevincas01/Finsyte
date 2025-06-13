b66b2e349f7e41b01d3ad1de8851bb650a3a70d1
Kevin Castro <kcapupp@princeton.edu>
2025-06-12 22:01:01 -0400

    Recurring payment list: allow modal edit on click button + show date

--- app/Components/Recurring/RecurringPaymentList.tsx
+++ app/Components/Recurring/RecurringPaymentList.tsx
@@ -1,10 +1,35 @@
+"use client";
 import { recurringExpenses } from "@/app/Constants/recurring";
 import { TransactionCategoryColors } from "@/app/Constants/transactions";
-import React from "react";
-
-const RecurringPaymentList = () => {
+import { formatDate } from "@/app/Utils/format";
+import React, { useState } from "react";
+import RecurringModal from "../Modal/RecurringModal";
+import { Recurring } from "@/app/Types/recurring";
+interface RecurringPaymentListProps {
+  expenses: Recurring[];
+}
+const RecurringPaymentList = ({ expenses }: RecurringPaymentListProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedRecurring, setSelectedRecurring] = useState<Recurring | null>(
+    null
+  );
   return (
     <div className="bg-white shadow-card rounded-md">
+      {isModalOpen && selectedRecurring && (
+        <RecurringModal
+          onClose={() => {
+            setIsModalOpen(false);
+            setSelectedRecurring(null);
+          }}
+          onSubmit={(budget) => {
+            console.log(budget);
+            setIsModalOpen(false);
+            setSelectedRecurring(null);
+          }}
+          mode="edit"
+          initialRecurring={selectedRecurring}
+        />
+      )}
       <div className="p-5 border-b border-b-gray-200">
         <p className="font-semibold">All Recurring Payments</p>
       </div>
@@ -25,11 +50,23 @@ const RecurringPaymentList = () => {
               {expense.category}
             </span>
           </div>
-          <div className="flex flex-row">
+          <div className="flex flex-row items-center gap-5">
             <div>
               <p className="font-medium">${expense.amount}</p>
-              <p className="text-sm text-gray-600">${expense.frequency}</p>
+              <p className="text-sm text-gray-600">{expense.frequency}</p>
             </div>
+            <p className="font-medium text-sm">
+              {formatDate(expense?.targetDate)}
+            </p>
+            <button
+              onClick={() => {
+                setSelectedRecurring(expense);
+                setIsModalOpen(true);
+              }}
+              className="text-blue-600 hover:underline text-sm cursor-pointer"
+            >
+              Edit
+            </button>{" "}
           </div>
         </div>
       ))}

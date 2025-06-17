81f297e149bf3d55e3c56ef2a34cd1a56b0bb0f4
Kevin Castro <kcapupp@princeton.edu>
2025-06-17 00:14:39 -0400

    Layout: Check user session + redirect if not logged in

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,15 +1,21 @@
+import { redirect } from "next/navigation";
 import SideBar from "../Components/SideBar";
+import { getUser, getUserSession } from "../Utils/Actions.ts/auth";
 
 export default async function AuthenticatedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const userSession = (await getUser()).data.user;
+  if (!userSession) {
+    redirect(`/auth`);
+  }
+
   return (
     <div className="">
       <div className="flex flex-row">
         <SideBar />
-
         <div className="p-5 flex flex-col flex-1 bg-gray-50">{children}</div>
       </div>
     </div>

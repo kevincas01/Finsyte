47095bc29be58517e61a78da1df0dcd6051259d1
Kevin Castro <kcapupp@princeton.edu>
2025-06-17 16:24:46 -0400

    Dashboard page: display user first name

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -3,13 +3,25 @@ import BillsCard from "@/app/Components/Dashboard/BillsCard";
 import DebtCard from "@/app/Components/Dashboard/DebtCard";
 import ExpensesCard from "@/app/Components/Dashboard/ExpensesCard";
 import RecentTransactionsCard from "@/app/Components/Dashboard/RecentTransactionsCard";
-const DashboardPage = () => {
+import { getUser } from "@/app/Utils/Actions.ts/auth";
+import { getUserInformation } from "@/app/Utils/Actions.ts/profiles";
+import { redirect } from "next/navigation";
+const DashboardPage = async () => {
+  const userSession = (await getUser()).data.user;
+
+  if (!userSession) {
+    redirect("/");
+  }
+  const profileInfo = (await getUserInformation(userSession.id))?.data;
+
   return (
     <div className="flex flex-col gap-5">
       <div className="">
         <h1 className="font-bold text-3xl">Dashboard</h1>
         <p className="text-gray-600 mt-1">
-          Welcome back! Here's your financial overview.
+          {`Welcome ${
+            profileInfo.first_name ? " " + profileInfo.first_name : ""
+          }! Here's your financial overview.`}
         </p>
       </div>
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-5">
@@ -19,7 +31,7 @@ const DashboardPage = () => {
         <BillsCard />
       </div>
 
-      <RecentTransactionsCard/>
+      <RecentTransactionsCard />
     </div>
   );
 };

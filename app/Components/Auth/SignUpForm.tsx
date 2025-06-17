5571a2d67b3cc6759dcf26ad191a91ef70b637e5
Kevin Castro <kcapupp@princeton.edu>
2025-06-17 00:14:08 -0400

    Signup form: handle signup function

--- app/Components/Auth/SignUpForm.tsx
+++ app/Components/Auth/SignUpForm.tsx
@@ -4,18 +4,53 @@ import GradientButton from "../Buttons/GradientButton";
 import PasswordInput from "../Inputs/PasswordInput";
 import TextInput from "../Inputs/TextInput";
 import GoogleButton from "./GoogleButton";
+import { signInUser, signUpUser } from "@/app/Utils/Actions.ts/auth";
+import { useSearchParams, useRouter } from "next/navigation";
+
 interface SignUpFormProps {
   onTabSwitch: () => void;
 }
 const SignUpForm = ({ onTabSwitch }: SignUpFormProps) => {
+  const searchParams = useSearchParams();
+  const originalRef = searchParams.get("originalRef");
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
+  const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const authData = { email, password };
+    const signUpResult = await signUpUser(authData);
 
-  const handleSignIn = () => {
-    console.log({ email, password });
+    if (!signUpResult.success) {
+      console.log(signUpResult)
+      setErrorMessage(
+        "This email is already registered as a user! Try Signing In!"
+      );
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 3000);
+      return;
+    }
+
+    const signInResult = await signInUser(authData);
+
+    if (!signInResult.success) {
+      setErrorMessage(
+        signInResult.error || "There was a problem with signing in. Try again!"
+      );
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 3000);
+      return;
+    }
+
+    router.push(originalRef ?? "/dashboard");
   };
+
   return (
-    <div className="p-10 flex flex-col gap-4 my-auto">
+    <form className="p-10 flex flex-col gap-4 h-full" onSubmit={handleSignUp}>
       <div className="text-center">
         <h2 className="font-semibold text-3xl">Sign Up</h2>
       </div>
@@ -38,8 +73,10 @@ const SignUpForm = ({ onTabSwitch }: SignUpFormProps) => {
         required={true}
       />
 
-      <GradientButton onClick={handleSignIn}>Sign Up</GradientButton>
-
+      <GradientButton type="submit">Sign Up</GradientButton>
+      {errorMessage && (
+        <p className="text-red-500 text-sm text-center">{errorMessage}</p>
+      )}
       <div className="flex items-center gap-4 w-full text-gray-500 text-sm">
         <div className="flex-grow border-t border-gray-300" />
         <span className="whitespace-nowrap">Or Continue with</span>
@@ -50,13 +87,14 @@ const SignUpForm = ({ onTabSwitch }: SignUpFormProps) => {
       <p className="text-center">
         Have an account?
         <button
+          type="button"
           className="text-primaryBlue font-medium ml-1 cursor-pointer"
           onClick={onTabSwitch}
         >
           Sign In
         </button>
       </p>
-    </div>
+    </form>
   );
 };
 

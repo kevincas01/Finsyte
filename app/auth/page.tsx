af0d81867caceb2370364ae5ffa1159aae09c437
Kevin Castro <kcapupp@princeton.edu>
2025-06-16 23:06:34 -0400

    Auth Page

---
+++ app/auth/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AuthPanel from '../Components/Auth/AuthPanel'
+
+const AuthPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <AuthPanel />
+    </div>
+  )
+}
+
+export default AuthPage

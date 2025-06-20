0155bbb4c6848f54e76ef1852f06638a1c3e8c0b
Kevin Castro <kcapupp@princeton.edu>
2025-06-19 22:52:59 -0400

    Create transactions function util

---
+++ app/Utils/Actions.ts/transactions.ts
@@ -0,0 +1,50 @@
+import { Transaction } from "plaid";
+import { createSupabaseServerClient } from "../Clients/supabaseClient";
+import { DBTransactionWithAccount } from "@/app/Types/transactions";
+
+interface CreateTransactionsParams {
+  userId: string;
+  itemId: string;
+  transactions: Transaction[];
+}
+export const createTransactions = async ({
+  userId,
+  itemId,
+  transactions,
+}: CreateTransactionsParams): Promise<{ success: boolean; error?: string }> => {
+  const supabase = await createSupabaseServerClient();
+
+  const transactionInserts = transactions.map((transaction) => {
+    const transactionData = {
+      user_id: userId,
+      item_id: itemId,
+      account_id: transaction.account_id,
+      amount: transaction.amount,
+      datetime: transaction.datetime,
+      name: transaction.merchant_name,
+      description: transaction.original_description,
+      pending: transaction.pending,
+      transaction_id: transaction.transaction_id,
+      logo_url: transaction.logo_url,
+      finance_category: transaction.personal_finance_category?.primary,
+    };
+
+    return supabase.from("transactions").insert(transactionData);
+  });
+
+  try {
+    const results = await Promise.all(transactionInserts);
+
+    const hasError = results.some((res) => res.error);
+
+    if (hasError) {
+      console.error("Error inserting one or more transactions", results);
+      return { success: false, error: "Error inserting one or more accounts" };
+    }
+
+    return { success: true };
+  } catch (err) {
+    console.error("Unexpected error inserting accounts:", err);
+    return { success: false, error: "Unexpected server error" };
+  }
+};

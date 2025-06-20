185ffeee70d226d854c4222ae404f5712f6b2b7f
Kevin Castro <kcapupp@princeton.edu>
2025-06-19 22:59:49 -0400

    Handle item sync function for accounts

--- app/Components/Accounts/ConnectedItemAccounts.tsx
+++ app/Components/Accounts/ConnectedItemAccounts.tsx
@@ -4,12 +4,34 @@ import NeutralButton from "../Buttons/NeutralButton";
 import SyncOutlinedIcon from "@mui/icons-material/SyncOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import { formatCurrency } from "@/app/Utils/format";
+import { getTransactionsFromItem } from "@/app/Utils/Actions.ts/plaid";
+import { createTransactions } from "@/app/Utils/Actions.ts/transactions";
 
 interface ConnectedItemAccountsProps {
   itemWithAccounts: PlaidItemWithAccounts;
 }
 
-const ConnectedItemAccounts = ({ itemWithAccounts }: ConnectedItemAccountsProps) => {
+const ConnectedItemAccounts = ({
+  itemWithAccounts,
+}: ConnectedItemAccountsProps) => {
+  const handleItemSync = async (itemId: string, userId: string) => {
+    // Handle sync: Sync transactions client.sync({})
+    // Returns latest transactions that have been added or modified
+
+    const transactions = (
+      await getTransactionsFromItem(itemWithAccounts.access_token)
+    ).transactions;
+
+    const transactionResponse = await createTransactions({
+      userId,
+      itemId,
+      transactions,
+    });
+    
+    // TODO
+    // Get latest sync for item and display it: use either a state or call db again
+  };
+
   return (
     <div className="bg-white rounded-xl border border-gray-200">
       <div className="flex flex-row justify-between items-center border-b border-gray-200 p-5 ">
@@ -21,11 +43,22 @@ const ConnectedItemAccounts = ({ itemWithAccounts }: ConnectedItemAccountsProps)
         </div>
 
         <div className="flex gap-3">
-          <NeutralButton onClick={() => {}} className="text-sm text-primaryBlue hover:bg-primaryBlue/10">
+          <NeutralButton
+            onClick={() => {
+              handleItemSync(
+                itemWithAccounts.item_id,
+                itemWithAccounts.user_id
+              );
+            }}
+            className="text-sm text-primaryBlue hover:bg-primaryBlue/10"
+          >
             <SyncOutlinedIcon fontSize="inherit" />
             <span className="ml-1">Sync</span>
           </NeutralButton>
-          <NeutralButton onClick={() => {}} className="text-red-500 hover:bg-red-500/10 text-sm">
+          <NeutralButton
+            onClick={() => {}}
+            className="text-red-500 hover:bg-red-500/10 text-sm"
+          >
             <DeleteOutlineOutlinedIcon fontSize="inherit" />
           </NeutralButton>
         </div>
@@ -37,7 +70,9 @@ const ConnectedItemAccounts = ({ itemWithAccounts }: ConnectedItemAccountsProps)
             key={account.account_id}
             className="border border-blue-100 bg-white rounded-lg p-4 flex flex-col gap-1 "
           >
-            <p className="font-medium text-gray-800">{account.name || "Unnamed Account"}</p>
+            <p className="font-medium text-gray-800">
+              {account.name || "Unnamed Account"}
+            </p>
             <p className="text-sm text-gray-600">
               {account.type} &mdash; {account.subtype}
             </p>

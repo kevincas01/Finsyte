060cf0a8c76db8fe75a8d79286055a904dfc2373
Kevin Castro <kcapupp@princeton.edu>
2025-06-19 23:11:10 -0400

    Transaction Page: retrieve user transactions with account

--- app/(authenticated)/transactions/page.tsx
+++ app/(authenticated)/transactions/page.tsx
@@ -1,13 +1,27 @@
 import TransactionsHeader from "@/app/Components/Transactions/TransactionsHeader";
 import TransactionsClient from "@/app/Components/Transactions/TransactionsClient";
-import { mockTransactions } from "@/app/Constants/transactions";
+import { getUser } from "@/app/Utils/Actions.ts/auth";
+import { redirect } from "next/navigation";
+import { getUserTransactionsWithAccount } from "@/app/Utils/Actions.ts/transactions";
+import { mapToClientTransaction } from "@/app/Utils/Transform/transactions";
 
-const TransactionsPage = () => {
-  const transactions = mockTransactions;
+const TransactionsPage = async () => {
+  const userSession = (await getUser()).data.user;
+
+  if (!userSession) {
+    redirect("/");
+  }
+  const userId = userSession.id;
+
+  const dbTransactions = (await getUserTransactionsWithAccount(userId)).data;
+  const transactions = dbTransactions!.map((transaction) =>
+    mapToClientTransaction(transaction)
+  );
+
   return (
     <div className="flex flex-col gap-5">
       <TransactionsHeader />
-      <TransactionsClient transactions={transactions} />
+      <TransactionsClient transactions={transactions!} />
     </div>
   );
 };

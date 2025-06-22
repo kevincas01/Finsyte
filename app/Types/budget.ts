ce799abe2fcb870276b9ce6d5f28e597447314e8
Kevin Castro <kcapupp@princeton.edu>
2025-06-21 21:56:17 -0400

    New budget types

--- app/Types/budget.ts
+++ app/Types/budget.ts
@@ -1,9 +1,22 @@
-export interface Budget {
-  description?: string;
+
+
+export interface DBBudget {
+  id: number;
+  user_id: string;
+  current_amount: number;
+  category: string;
+  budget_amount: number;
+  period: BudgetPeriodCategory;
+  deadline_date: string;
+}
+export interface ClientBudget {
+  id: number;
+  userId: string;
   currentAmount: number;
-  category:string;
+  financeCategory: string;
   budgetAmount: number;
   period: BudgetPeriodCategory;
+  deadlineDate: string;
 }
 
-export type BudgetPeriodCategory =  "Monthly" | "Quarterly" | "Yearly";
+export type BudgetPeriodCategory = "Monthly" | "Quarterly" | "Yearly";

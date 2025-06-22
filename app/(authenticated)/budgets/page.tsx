0c5ebb5eede523e186aa91ec83f33901cd977518
Kevin Castro <kcapupp@princeton.edu>
2025-06-21 22:13:46 -0400

    Budget Page: Retrieve user budgets with user id

--- app/(authenticated)/budgets/page.tsx
+++ app/(authenticated)/budgets/page.tsx
@@ -1,12 +1,28 @@
 import BudgetHeader from "@/app/Components/Budgets/BudgetHeader";
 import BudgetPeriodDisplay from "@/app/Components/Budgets/BudgetPeriodDisplay";
 import { exampleBudgets } from "@/app/Constants/budgets";
+import { getUser } from "@/app/Utils/Actions.ts/auth";
+import { getUserBudgets } from "@/app/Utils/Actions.ts/budgets";
+import { mapToClientBudget } from "@/app/Utils/Transform/budgets";
+import { redirect } from "next/navigation";
 
-const BudgetsPage = () => {
+const BudgetsPage = async() => {
+  const userSession = (await getUser()).data.user;
+
+  if (!userSession) {
+    redirect("/");
+  }
+  const userId = userSession.id;
+  const userBudgets = (await getUserBudgets(userId)).data;
+  const budgets = userBudgets!.map((budget) =>
+    mapToClientBudget(budget)
+  );
+
+  getUserBudgets
   return (
     <div className="flex flex-col gap-5">
       <BudgetHeader />
-      <BudgetPeriodDisplay budgets={exampleBudgets} />
+      <BudgetPeriodDisplay budgets={budgets} />
     </div>
   );
 };

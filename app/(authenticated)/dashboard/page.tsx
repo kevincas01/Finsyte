f062b07f49d0b0f2bbce4f9e075db7b2cd7e28f9
Kevin Castro <kcapupp@princeton.edu>
2025-06-24 21:48:28 -0400

    Dashboard Page: retrieve accounts + get data for total balance and expenses to pass as props

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -3,9 +3,11 @@ import BillsCard from "@/app/Components/Dashboard/BillsCard";
 import DebtCard from "@/app/Components/Dashboard/DebtCard";
 import ExpensesCard from "@/app/Components/Dashboard/ExpensesCard";
 import RecentTransactionsCard from "@/app/Components/Dashboard/RecentTransactionsCard";
+import { getUserAccounts } from "@/app/Utils/Actions.ts/accounts";
 import { getUser } from "@/app/Utils/Actions.ts/auth";
 import { getUserInformation } from "@/app/Utils/Actions.ts/profiles";
 import { getUserTransactionsWithAccount } from "@/app/Utils/Actions.ts/transactions";
+import { getMonthlyExpensesTotal } from "@/app/Utils/expenses";
 import { mapToClientTransaction } from "@/app/Utils/Transform/transactions";
 import { redirect } from "next/navigation";
 const DashboardPage = async () => {
@@ -15,11 +17,21 @@ const DashboardPage = async () => {
     redirect("/");
   }
   const userId = userSession.id;
-  const profileInfo = (await getUserInformation(userSession.id))?.data;
+  const profileInfo = (await getUserInformation(userId))?.data;
   const dbTransactions = (await getUserTransactionsWithAccount(userId)).data;
   const transactions = dbTransactions!.map((transaction) =>
     mapToClientTransaction(transaction)
   );
+
+  const accountsInfo = (await getUserAccounts(userId)).data;
+
+  const expenseReport = getMonthlyExpensesTotal(transactions, true);
+
+  const totalBalance = accountsInfo?.reduce(
+    (total, account) => total + account.available_balance,
+    0
+  );
+
   return (
     <div className="flex flex-col gap-5">
       <div className="">
@@ -31,8 +43,14 @@ const DashboardPage = async () => {
         </p>
       </div>
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-5">
-        <BalancesCard />
-        <ExpensesCard />
+        <BalancesCard
+          totalBalance={totalBalance!}
+          accountsLength={accountsInfo?.length!}
+        />
+        <ExpensesCard
+          expensesTotal={expenseReport.total}
+          topExpense={expenseReport.topCategory!}
+        />
         <DebtCard />
         <BillsCard />
       </div>

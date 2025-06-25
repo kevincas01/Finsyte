62b5f92d548ffc90e360a7a9dd562c97a26dd84d
Kevin Castro <kcapupp@princeton.edu>
2025-06-24 21:45:56 -0400

    Balances Card: accept props to display data

--- app/Components/Dashboard/BalancesCard.tsx
+++ app/Components/Dashboard/BalancesCard.tsx
@@ -1,18 +1,24 @@
+import { formatCurrency } from "@/app/Utils/format";
 import AttachMoneyOutlinedIcon from "@mui/icons-material/AttachMoneyOutlined";
-
-const BalancesCard = () => {
+interface BalancesCardProps {
+  totalBalance: number;
+  accountsLength: number;
+}
+const BalancesCard = ({ totalBalance, accountsLength }: BalancesCardProps) => {
   return (
     <div className="p-5 bg-white rounded-md shadow-card">
       <div className="flex flex-row justify-between items-center">
         <div className="bg-green-500 text-white p-2 rounded-md">
           <AttachMoneyOutlinedIcon />
         </div>
-        <p className="text-green-600 text-sm"> +$1,244</p>
+        {/* <p className="text-green-600 text-sm"> +$1,244</p> */}
       </div>
       <div className="mt-4">
         <h3 className="text-gray-600 text-sm font-medium">Total Balance</h3>
-        <p className="font-bold text-2xl">$24,534</p>
-        <p className="text-gray-400 text-xs mt-1">Across 3 accounts</p>
+        <p className="font-bold text-2xl">{formatCurrency(totalBalance)}</p>
+        <p className="text-gray-400 text-xs mt-1">
+          Across {accountsLength} account{accountsLength>1 &&"s"}
+        </p>
       </div>
     </div>
   );

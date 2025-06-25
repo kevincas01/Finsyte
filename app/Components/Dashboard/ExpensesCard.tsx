6bcaca63dcf5fb5bf6fcf8467a1a60c8996cc007
Kevin Castro <kcapupp@princeton.edu>
2025-06-24 21:45:46 -0400

    Expense Card: accept props to display data

--- app/Components/Dashboard/ExpensesCard.tsx
+++ app/Components/Dashboard/ExpensesCard.tsx
@@ -1,18 +1,24 @@
+import { formatCurrency } from "@/app/Utils/format";
 import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
-
-const ExpensesCard = () => {
+interface ExpensesCardProps {
+  expensesTotal: number;
+  topExpense: { category: string; amount: number };
+}
+const ExpensesCard = ({ expensesTotal, topExpense }: ExpensesCardProps) => {
   return (
     <div className="p-5 bg-white rounded-md shadow-card">
       <div className="flex flex-row justify-between items-center">
         <div className="bg-yellow-500 text-white p-2 rounded-md">
           <ReceiptOutlinedIcon />
         </div>
-        <p className="text-green-600 text-sm"> +$244</p>
+        {/* <p className="text-green-600 text-sm"> +$244</p> */}
       </div>
       <div className="mt-4">
         <h3 className="text-gray-600 text-sm font-medium">Monthly Expenses</h3>
-        <p className="font-bold text-2xl">$2,534</p>
-        <p className="text-gray-400 text-xs mt-1">Top: Food ($764)</p>
+        <p className="font-bold text-2xl">{formatCurrency(expensesTotal)}</p>
+        <p className="text-gray-400 text-xs mt-1">
+          Top: {topExpense.category} ({formatCurrency(topExpense.amount)})
+        </p>
       </div>
     </div>
   );

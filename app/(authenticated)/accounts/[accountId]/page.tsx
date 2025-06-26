91c886af529f4ed999883e4dfceb626ed9559ff0
Kevin Castro <kcapupp@princeton.edu>
2025-06-25 22:30:18 -0400

    Account id Page

---
+++ app/(authenticated)/accounts/[accountId]/page.tsx
@@ -0,0 +1,32 @@
+import AccountDetails from "@/app/Components/Accounts/Single Account Page/AccountDetails";
+import { getAccountInfoWithAccountId } from "@/app/Utils/Actions.ts/accounts";
+import { getUser } from "@/app/Utils/Actions.ts/auth";
+import { getUserTransactionsWithAccountId } from "@/app/Utils/Actions.ts/transactions";
+import { mapToClientTransaction } from "@/app/Utils/Transform/transactions";
+import { redirect } from "next/navigation";
+
+interface Props {
+  params: {
+    accountId: string;
+  };
+}
+
+export default async function AccountPage({ params }: Props) {
+  const userSession = (await getUser()).data.user;
+  if (!userSession) redirect("/");
+
+  const userId = userSession.id;
+  const accountId = params.accountId;
+
+  const account = (await getAccountInfoWithAccountId(userId, accountId))?.data;
+  if (!account) redirect("/dashboard");
+
+  const dbTransactions = (
+    await getUserTransactionsWithAccountId(userId, accountId)
+  ).data;
+  const transactions = dbTransactions!.map((transaction) =>
+    mapToClientTransaction(transaction)
+  );
+
+  return <AccountDetails account={account} transactions={transactions} />;
+}

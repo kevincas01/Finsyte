c2593b3fcce9198005cf7b3b40f17ec9d5a323b7
Kevin Castro <kcapupp@princeton.edu>
2025-06-25 22:14:39 -0400

    Recent transactions card: replace code with transaction list component

--- app/Components/Dashboard/RecentTransactionsCard.tsx
+++ app/Components/Dashboard/RecentTransactionsCard.tsx
@@ -1,5 +1,5 @@
-import { TransactionCategoryColors } from "@/app/Constants/transactions";
 import { ClientTransaction } from "@/app/Types/transactions";
+import TransactionsList from "../TransactionsList";
 
 interface RecentTransactionsCardProps {
   transactions: ClientTransaction[];
@@ -17,44 +17,7 @@ const RecentTransactionsCard = ({
       </div>
       <div></div>
       {transactions.map((transaction, index) => (
-        <div key={index} className="flex items-center justify-between p-3">
-          <div className="flex items-center">
-            <div className="w-10 h-10 bg-gray-100 rounded-full flex items-center justify-center mr-3">
-              <span className="text-gray-600 text-sm">
-                {transaction.name[0]}
-              </span>
-            </div>
-            <div>
-              <p className="font-medium">{transaction.name}</p>
-              <span
-                className={`text-xs font-medium px-2 py-1 rounded-full ${
-                  TransactionCategoryColors[transaction.financeCategory!]
-                }`}
-              >
-                {transaction.financeCategory}
-              </span>
-            </div>
-          </div>
-          <div className="text-right">
-            {transaction.amount > 0 ? (
-              <p className="font-semibold text-red-500">
-                -${transaction.amount.toFixed(2)}
-              </p>
-            ) : (
-              <p className="font-semibold text-green-500">
-                +${(-1 * transaction.amount).toFixed(2)}
-              </p>
-            )}
-
-            <p className="text-sm text-gray-500">
-              {new Date(transaction.datetime).toLocaleDateString("en-US", {
-                month: "short",
-                day: "numeric",
-                year: "numeric",
-              })}
-            </p>
-          </div>
-        </div>
+        <TransactionsList transaction={transaction} key={index} />
       ))}
     </div>
   );

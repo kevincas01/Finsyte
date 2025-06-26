4c13aadc831989630135e1a07f95fd447ee05ccb
Kevin Castro <kcapupp@princeton.edu>
2025-06-25 22:20:27 -0400

    Recent transactions card: route push to transactions page

--- app/Components/Dashboard/RecentTransactionsCard.tsx
+++ app/Components/Dashboard/RecentTransactionsCard.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { ClientTransaction } from "@/app/Types/transactions";
 import TransactionsList from "../TransactionsList";
+import { useRouter } from "next/navigation";
 
 interface RecentTransactionsCardProps {
   transactions: ClientTransaction[];
@@ -7,11 +9,17 @@ interface RecentTransactionsCardProps {
 const RecentTransactionsCard = ({
   transactions,
 }: RecentTransactionsCardProps) => {
+  const router = useRouter();
   return (
     <div className="p-5 bg-white shadow-card rounded-md">
       <div className="flex flex-row justify-between mb-5">
         <h4 className="text-xl font-semibold">Recent Transactions</h4>
-        <button className="text-primaryBlue font-medium ml-1 cursor-pointer">
+        <button
+          onClick={() => {
+            router.push("/transactions");
+          }}
+          className="text-primaryBlue font-medium ml-1 cursor-pointer"
+        >
           View All
         </button>
       </div>

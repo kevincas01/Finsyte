f7e613aaeb3f8ea6a0fcd98026469df9364fc35c
Kevin Castro <kcapupp@princeton.edu>
2025-06-26 23:07:23 -0400

    Neutral button: add variant prop

--- app/Components/Buttons/NeutralButton.tsx
+++ app/Components/Buttons/NeutralButton.tsx
@@ -8,6 +8,7 @@ interface NeutralButtonProps {
   disabled?: boolean;
   loading?: boolean;
   className?: string;
+  variant?: "gray" | "white";
 }
 
 const NeutralButton = ({
@@ -17,15 +18,18 @@ const NeutralButton = ({
   disabled = false,
   loading = false,
   className = "",
+  variant = "white",
 }: NeutralButtonProps) => {
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={disabled || loading}
-      className={`cursor-pointer text-center text-black rounded-md font-medium px-4 py-2 transition duration-200 ease-in-out 
-      bg-white border border-gray-200 hover:bg-gray-100 flex gap-2 flex-1 justify-center items-center
+      className={`cursor-pointer text-center  rounded-md font-medium px-4 py-2 transition duration-200 ease-in-out 
+      flex gap-2 flex-1 justify-center items-center 
       ${disabled || loading ? "opacity-50 cursor-not-allowed" : ""} 
+
+      ${variant === "white" ? "bg-white hover:bg-gray-100 text-black  border border-gray-200" : "bg-gray-100 hover:bg-gray-200 text-black "}
       ${className}`}
       aria-busy={loading}
     >

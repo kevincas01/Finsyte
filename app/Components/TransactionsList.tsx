47998849c71cd10c34ff15b2ab5db2cac5021e7d
Kevin Castro <kcapupp@princeton.edu>
2025-06-26 22:42:18 -0400

    Transactions List: Allow Multiple transactions

--- app/Components/TransactionsList.tsx
+++ app/Components/TransactionsList.tsx
@@ -4,51 +4,57 @@ import Image from "next/image";
 import { TransactionCategoryColors } from "../Constants/transactions";
 import { formatDateToMMDDYYYY } from "../Utils/format";
 interface TransactionsListProps {
-  transaction: ClientTransaction;
+  transactions: ClientTransaction[];
 }
-const TransactionsList = ({ transaction }: TransactionsListProps) => {
+const TransactionsList = ({ transactions }: TransactionsListProps) => {
   return (
-    <div className="flex items-center justify-between p-3">
-      <div className="flex items-center">
-        <div className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center mr-3 relative overflow-hidden">
-          {transaction.logoUrl ? (
-            <Image
-              fill
-              src={transaction.logoUrl}
-              alt="Merchant Logo"
-              objectFit="cover"
-              className="relative rounded-full"
-            />
-          ) : (
-            <span className="text-gray-600 text-lg">{transaction.name[0]}</span>
-          )}
-        </div>
-        <div>
-          <p className="font-medium">{transaction.name}</p>
-          <span
-            className={`text-xs font-medium px-2 py-1 rounded-full ${
-              TransactionCategoryColors[transaction.financeCategory!]
-            }`}
-          >
-            {transaction.financeCategory}
-          </span>
-        </div>
-      </div>
-      <div className="text-right">
-        {transaction.amount > 0 ? (
-          <p className="font-semibold text-red-500">
-            -${transaction.amount.toFixed(2)}
-          </p>
-        ) : (
-          <p className="font-semibold text-green-500">
-            +${(-1 * transaction.amount).toFixed(2)}
-          </p>
-        )}
+    <div className="overflow-auto flex flex-col gap-2 box-border flex-1">
+      {transactions.map((transaction, index) => (
+        <div key={index} className="flex items-center justify-between p-3">
+          <div className="flex items-center">
+            <div className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center mr-3 relative overflow-hidden">
+              {transaction.logoUrl ? (
+                <Image
+                  fill
+                  src={transaction.logoUrl}
+                  alt="Merchant Logo"
+                  objectFit="cover"
+                  className="relative rounded-full"
+                />
+              ) : (
+                <span className="text-gray-600 text-lg">
+                  {transaction.name[0]}
+                </span>
+              )}
+            </div>
+            <div>
+              <p className="font-medium">{transaction.name}</p>
+              <span
+                className={`text-xs font-medium px-2 py-1 rounded-full ${
+                  TransactionCategoryColors[transaction.financeCategory!]
+                }`}
+              >
+                {transaction.financeCategory}
+              </span>
+            </div>
+          </div>
+          <div className="text-right">
+            {transaction.amount > 0 ? (
+              <p className="font-semibold text-red-500">
+                -${transaction.amount.toFixed(2)}
+              </p>
+            ) : (
+              <p className="font-semibold text-green-500">
+                +${(-1 * transaction.amount).toFixed(2)}
+              </p>
+            )}
 
-        <p className="text-sm text-gray-500">
-          {formatDateToMMDDYYYY(transaction.datetime)}
-        </p>
-      </div>
+            <p className="text-sm text-gray-500">
+              {formatDateToMMDDYYYY(transaction.datetime)}
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };

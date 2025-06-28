d5c70bc1e715fa4ab4f2980ebce2a676ba3cb05b
Kevin Castro <kcapupp@princeton.edu>
2025-06-27 23:27:12 -0400

    Get User Recurrig Transaction function util

--- app/Utils/Actions.ts/recurring.ts
+++ app/Utils/Actions.ts/recurring.ts
@@ -1,6 +1,8 @@
 "use server";
 import { TransactionStream } from "plaid";
 import { createSupabaseServerClient } from "../Clients/supabaseClient";
+import { DBRecurringTransaction } from "@/app/Types/recurring";
+import { mapPlaidCategoryToCustom } from "../categories";
 
 export const upsertRecurringOutflows = async (
   userId: string,
@@ -18,13 +20,33 @@ export const upsertRecurringOutflows = async (
     next_date: outflow.predicted_next_date,
   }));
 
-  const { error } = await supabase
-    .from("recurring")
-    .upsert(formattedOutflows, {
-      onConflict: "stream_id",
-    });
+  const { error } = await supabase.from("recurring").upsert(formattedOutflows, {
+    onConflict: "stream_id",
+  });
 
   if (error) {
     console.error("Upsert error:", error);
   }
+};
+
+export const getUserRecurringTransactions = async (
+  userId: string
+): Promise<{
+  success: boolean;
+  data?: DBRecurringTransaction[];
+  error?: string;
+}> => {
+  const supabase = await createSupabaseServerClient();
+
+  const { data, error } = await supabase
+    .from("recurring")
+    .select("*")
+    .eq("user_id", userId);
+
+  if (error) {
+    console.error("Failed to fetch user goals:", error.message);
+    return { success: false, error: error.message };
+  }
+
+  return { success: true, data };
 };

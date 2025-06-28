6214dd23c818c768ffb40981d0d17d0be989edd3
Kevin Castro <kcapupp@princeton.edu>
2025-06-27 23:29:39 -0400

    mapToClientRecurring function util

---
+++ app/Utils/Transform/recurring.ts
@@ -0,0 +1,18 @@
+import {
+  ClientRecurringTransaction,
+  DBRecurringTransaction,
+} from "@/app/Types/recurring";
+
+export const mapToClientRecurring = (
+  recurring: DBRecurringTransaction
+): ClientRecurringTransaction => {
+  return {
+    id: recurring.id,
+    category: recurring.category,
+    name: recurring.name,
+    amount: recurring.amount,
+    frequency: recurring.frequency,
+    last_date: recurring.last_date,
+    next_date: recurring.next_date,
+  };
+};

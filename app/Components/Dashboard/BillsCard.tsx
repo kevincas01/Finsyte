21d28ba2d22988ff3cd9c7fe90c8f38792fbadbc
Kevin Castro <kcapupp@princeton.edu>
2025-07-03 22:12:06 -0400

    Bills card: Allow props + display them

--- app/Components/Dashboard/BillsCard.tsx
+++ app/Components/Dashboard/BillsCard.tsx
@@ -1,6 +1,13 @@
+import { formatCurrency } from "@/app/Utils/format";
 import EventNoteOutlinedIcon from "@mui/icons-material/EventNoteOutlined";
-
-const BillsCard = () => {
+interface BillsCardProps {
+  totalUpcomingBalance: number;
+  upcomingBalanceLength: number;
+}
+const BillsCard = ({
+  totalUpcomingBalance,
+  upcomingBalanceLength,
+}: BillsCardProps) => {
   return (
     <div className="p-5 bg-white rounded-md shadow-card">
       <div className="flex flex-row justify-between items-center">
@@ -10,8 +17,13 @@ const BillsCard = () => {
       </div>
       <div className="mt-4">
         <h3 className="text-gray-600 text-sm font-medium">Upcoming Bills</h3>
-        <p className="font-bold text-2xl">$3,456</p>
-        <p className="text-gray-400 text-xs mt-1">5 bills due this week</p>
+        <p className="font-bold text-2xl">
+          {formatCurrency(totalUpcomingBalance)}
+        </p>
+        <p className="text-gray-400 text-xs mt-1">
+          {upcomingBalanceLength} bill{upcomingBalanceLength > 1 && "s"} due
+          this week
+        </p>
       </div>
     </div>
   );
